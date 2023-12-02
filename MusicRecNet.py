378c09d33bcf31bf5b423903db33e2d4fecd15d4
Samantha <samanthahawco@gmail.com>
2023-12-02 15:40:40 -0500

    adding replicated model

---
+++ MusicRecNet.py
@@ -0,0 +1,52 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+# CNN model based on model constructed by A. Elbir, N. Aydin
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout_rate = 0.2):
+        super(ConvBlock, self).__init__()
+
+        self.layerBlock = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, (3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=(0, 0), ceil_mode=True),
+            nn.ReflectionPad2d((1, 1, 1, 1)),
+            nn.Dropout(dropout_rate)
+        )
+
+        def forward (self, X):
+            return self.layerBlock(X)
+        
+class MusicClassNet(nn.Module):
+    def __init__(self, base):
+        super(MusicClassNet, self).__init__()
+
+        # 3 layers of convolution, max pooling, and dropout
+        self.layer1 = ConvBlock(in_channels=3, out_channels=32, dropout_rate=0.2)
+        self.layer2 = ConvBlock(in_channels=32, out_channels=64, dropout_rate=0.2)
+        self.layer3 = ConvBlock(in_channels=64, out_channels=128, dropout_rate=0.2)
+
+        # Flatten layer
+        self.flatten = nn.Flatten()
+
+        # Fully connected layers
+        self.dense = nn.Linear(16*16*128, 128)  # input size based on output of flatten layer
+        self.dense_2 = nn.Linear(128, 10)         # output of size 10 for 10 genre classes
+        # Initialize weights
+        self.init_weights()
+
+    def forward (self, x):
+        x = self.layer3(self.layer2(self.layer1(x)))
+        x = self.flatten(x)
+        x = F.relu(self.dense(x))
+        x = self.dense_2(x)
+
+        if not self.training:
+            x = F.softmax(x, dim=1)
+
+    def init_weights(self):
+        # initialize weights of model
+        pass
